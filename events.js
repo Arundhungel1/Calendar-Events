fc2fc80aad11e06f862b9742332d065269706387
Arun Dhungel <arundhungel.npa@gmail.com>
2019-06-13 08:33:29 +1000

    creating variables

---
+++ events.js
@@ -0,0 +1,21 @@
+let already_booked = [
+{start: '2018-12-11 16:00:00', end: '2018-12-11 17:00:00'},
+{start: '2018-12-20 9:00:00', end: '2018-12-20 10:00:00'},
+{start: '2018-12-21 13:00:00', end: '2018-12-21 13:30:00'},
+
+];
+let available = [
+{start: '2018-12-19 16:00:00', end: '2018-12-19 17:00:00'},
+{start: '2018-12-20 9:30:00', end: '2018-12-20 11:30:00'},
+{start: '2018-12-28 13:00:00', end: '2018-12-28 15:00:00'},
+{start: '2018-12-29 13:00:00', end: '2018-12-29 14:00:00'}];
+
+function getAvailableDayTime(already_booked, available) {
+  let canBook = available.slice();
+
+  canBook.forEach((x,j) => {
+    already_booked.forEach((y,i) => {
+      
+      })
+    })
+}
